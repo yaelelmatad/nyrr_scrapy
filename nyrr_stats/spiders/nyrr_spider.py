e3d9db6cdfa920813784c5b648a5d3e372414134
yaelelmatad <yael.elmatad@gmail.com>
2012-07-17 17:27:13 -0400

    race importing starting to work

--- nyrr_stats/spiders/nyrr_spider.py
+++ nyrr_stats/spiders/nyrr_spider.py
@@ -1,6 +1,8 @@
 from scrapy.spider import BaseSpider
 from scrapy.http import FormRequest
+from scrapy.http import Request
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
 
 class NYRRSpider(BaseSpider):
     linkextractor = SgmlLinkExtractor("result\.id=")
@@ -12,7 +14,7 @@ class NYRRSpider(BaseSpider):
     
     def parse(self, response):
         req = []
-        for i in range(2007,2012):
+        for i in range(2007,2008):
             req.append(FormRequest.from_response(response,
                 formdata = {"NYRRYEAR":str(i)}, 
                 callback=self.parse2))
@@ -20,5 +22,20 @@ class NYRRSpider(BaseSpider):
         return req;
 
     def parse2(self, response): 
-        print self.linkextractor.extract_links(response)
-        
+        req=[]
+        for i in self.linkextractor.extract_links(response):
+            req.append(Request(i.url, callback = self.parseRaces))
+        return req
+
+    def parseRaces(self, response):
+        req = FormRequest.from_response(response,
+            formdata = {"items.display":"500"},
+            callback=self.parseRace)
+        return req
+    
+    def parseRace(self,response):
+        hxs = HtmlXPathSelector(response);
+        racename =  hxs.select('//title/text()').extract()
+        metadata = hxs.select("//span[@class='text']").extract()[0].split('<br>')
+        print metadata
+        #raceinfo = hxs.select('//
