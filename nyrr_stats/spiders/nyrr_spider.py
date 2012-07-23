8fc33c13a543b889f0fe90ac435fba45d89bf0e3
yaelelmatad <yael.elmatad@gmail.com>
2012-07-23 17:47:41 -0400

    all works

--- nyrr_stats/spiders/nyrr_spider.py
+++ nyrr_stats/spiders/nyrr_spider.py
@@ -4,7 +4,6 @@ from scrapy.http import Request
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
 import re
-#from BeautifulSoup import BeautifulSoup
 from nyrr_stats.items import NyrrStatsItem
 
 humidity_re = re.compile(r'([0-9]+)%',re.UNICODE)
@@ -13,13 +12,13 @@ weather_uni_re = re.compile(',+? ([0-9]+).*?F', re.UNICODE)
 gender_age_re = re.compile('(.)([0-9]+)', re.UNICODE)
 metadata_re = re.compile(r'<b>(.+?):.*?</b>(.+?)<br>',re.UNICODE)
 #.+ means anything with any number of characters (+), sepearated by <br> -- any text that isn't special, r = raw string
-m_raceName = "" 
-m_location = ""
-m_distMiles = ""
-m_date = ""
-m_temp = ""
-m_humidity = ""
-dataLocation = []
+#m_raceName = "" 
+#m_location = ""
+#m_distMiles = ""
+#m_date = ""
+#m_temp = ""
+#m_humidity = ""
+#dataLocation = []
 
 class NYRRSpider(BaseSpider):
     linkextractor = SgmlLinkExtractor("result\.id=")
@@ -32,7 +31,7 @@ class NYRRSpider(BaseSpider):
 
     def parse(self, response):
         req = []
-        for i in range(2007,2008):
+        for i in range(1990,1995):
             req.append(FormRequest.from_response(response,
                 formdata = {"NYRRYEAR":str(i)}, 
                 callback=self.parse2))
@@ -42,7 +41,7 @@ class NYRRSpider(BaseSpider):
     def parse2(self, response): 
         req=[]
         #for x in self.linkextractor.extract_links(response):
-        for x in self.linkextractor.extract_links(response)[:3]:
+        for x in self.linkextractor.extract_links(response):
             req.append(Request(x.url, callback = self.parseRaces))
         return req
 
@@ -52,32 +51,36 @@ class NYRRSpider(BaseSpider):
             callback=self.parseRace)
         return req
     
-    def parseRace(self,response):
-        global m_temp
-        global m_humidity
-        global m_raceName
-        global m_date
-        global m_distMiles
-        global m_location 
-
+    def parseRace(self, response):
+        item = []
+        req = []
+        flag = 1
+        for x in self.linkextractor2.extract_links(response):
+            if (x.text =='NEXT 500' and flag == 1):
+                flag = 0 #only want to call the next link one time and "next 500" appears 2x on page
+                req.append(Request(x.url, callback = self.parseRace))
+                
         hxs = HtmlXPathSelector(response)
         rn =  hxs.select('//title/text()').extract()
         m_raceName = rn[0]
+        #print m_raceName
         metadata = hxs.select("//span[@class='text']").extract()[0]
         parsedMetadata =  metadata_re.findall(metadata)
         for name, value in parsedMetadata:
-            m_distMiles = "" #IN CASE THES DON'T EXIST IN DATA
-            m_date = ""
-            m_temp = ""
-            m_humidity =""
-            m_location = ""
+        #    m_distMiles = "" #IN CASE THES DON'T EXIST IN DATA
+        #    m_date = ""
+        #    m_temp = ""
+        #    m_humidity =""
+        #    m_location = ""
 
             if name=='Distance':
                 m_distMiles = value.split(" ")[0]
+                #print m_distMiles
 
             if name=='Date/Time':
                 m_date = value
-    
+                #print m_date
+
             if name=='Weather':
                 parsedWeatherUni = weather_uni_re.findall(value)
                 if parsedWeatherUni:
@@ -88,7 +91,7 @@ class NYRRSpider(BaseSpider):
                         m_temp = parsedWeatherDeg[0]
                     else:
                         m_temp =""
-                
+        
                 parsedHumidity = humidity_re.findall(value)
                 if parsedHumidity:
                     m_humidity = parsedHumidity[0]
@@ -97,8 +100,35 @@ class NYRRSpider(BaseSpider):
 
             if name=='Location':
                 m_location = value   
+                #print m_location
 
-        global dataLocation
+        try: 
+            m_distMiles
+        except noDistance: 
+            m_distMiles = ""
+
+        try:
+            m_date
+        except noDate:
+            m_date = ""
+
+        try:
+            m_temp
+        except noTemp:
+            m_temp =""
+
+        try:
+            m_humidity
+        except noHumidity:
+            m_humidity = ""
+        
+        try:
+            m_location
+        except noLocation:
+            m_location=""
+        
+        dataLocation = []
+
         for j in range(0,20):
             dataLocation.append(0) #zero will always be an empty string
 
@@ -106,7 +136,6 @@ class NYRRSpider(BaseSpider):
         i=1;
 
         while (header): #go through column by column
-            #print header
             headerStripped = [j for j in header if j != u'\xa0'] #get rid of the garbage nbsp
             headerConcat = ' '.join(headerStripped) #concatinate
             dataLocation[(self.whichDataMember(headerConcat))]=i
@@ -118,7 +147,7 @@ class NYRRSpider(BaseSpider):
             runnerData.append("")
 
         data  = hxs.select('//table[2]/tr[2]/td[1]/text()').extract()
-	print data
+        #print data
         k = 2
         while(data):
             i=1
@@ -128,107 +157,44 @@ class NYRRSpider(BaseSpider):
                 runnerData[i] = dataConcat.lower() #save all as lower case
                 i = i + 1
                 data =  hxs.select('//table[2]/tr[' + str(k) + ']/td[' + str(i) + ']/text()').extract()
-		print data
-				
-            #need to split up gender age data
-            genderAge = gender_age_re.findall(runnerData[dataLocation[3]])
-            m_gender = genderAge[0][0]
-            m_age = genderAge[0][1]
-
-            item = NyrrStatsItem(raceName = rn[0].lower(), location=m_location.lower(), distMiles = m_distMiles.lower(), date = m_date.lower(), temp = m_temp.lower(), 
-                        humidity = m_humidity.lower(),
-                        lastName = runnerData[dataLocation[1]],
-                        firstName = runnerData[dataLocation[2]],
-                        sex = m_gender,
-                        age = m_age,
-                        bib = runnerData[dataLocation[4]],
-                        team = runnerData[dataLocation[5]],
-                        city = runnerData[dataLocation[6]],
-                        state = runnerData[dataLocation[7]],
-                        country = runnerData[dataLocation[8]],
-                        overallPlace= runnerData[dataLocation[9]],
-                        genderPlace= runnerData[dataLocation[10]],
-                        agePlace= runnerData[dataLocation[11]],
-                        netTime= runnerData[dataLocation[12]],
-                        pacePerMile= runnerData[dataLocation[13]],
-                        AGTime= runnerData[dataLocation[14]],
-                        AGGenderPlace= runnerData[dataLocation[15]],
-                        AGPercent= runnerData[dataLocation[16]],
-                        )
-            yield item
-
-            k = k + 1
-            data =  hxs.select('//table[2]/tr[' + str(k) + ']/td[1]/text()').extract()
-	    print data
-
-
-        #parseRunnersReturned = self.parseRunners(response)
-        return 
-        #return parseRunnersReturned + [item]
-    
-    
-    
-    def parseRunners(self, response):
-        req = []
-        for x in self.linkextractor2.extract_links(response):
-            print x.text
-            if (x.text =='NEXT 500'):
-                print m_raceName
-                req.append(Request(x.url, callback = self.parseRunners))
-                parsedOnePageReturned = self.parseOnePageRunner(response)                
-                break 
-        
-        return [req] + parsedOnePageReturned
-
-    def parseOnePageRunner(self, response):
-        print dataLocation
-        print m_raceName
-        hxs = HtmlXPathSelector(response)
-        runnerData = []
-        for j in range(0,30):
-            runnerData.append("")
-
-        data  = hxs.select('//table[2]/tr[2]/td[1]/text()').extract()
-        k = 2
-        while(data):
-            i=1
-            while(data):
-                dataStripped = [j for j in data if j != u'\xa0'] #get rid of the garbage nbsp
-                dataConcat = ' '.join(dataStripped) #concatinate
-                runnerData[i] = dataConcat.lower() #save all as lower case
-                i = i + 1
-                data =  hxs.select('//table[2]/tr[' + str(k) + ']/td[' + str(i) + ']/text()').extract()
-
+                #print data
+                        
                 #need to split up gender age data
-            if (runnerData[dataLocation[3]]):
+            if (runnerData[dataLocation[3]]!=""):
                 genderAge = gender_age_re.findall(runnerData[dataLocation[3]])
                 m_gender = genderAge[0][0]
                 m_age = genderAge[0][1]
 
-            item = NyrrStatsItem(raceName = rn[0].lower(), location=m_location.lower(), distMiles = m_distMiles.lower(), date = m_date.lower(), temp = m_temp.lower(), 
-                        humidity = m_humidity.lower(),
-                        lastName = runnerData[dataLocation[1]],
-                        firstName = runnerData[dataLocation[2]],
-                        sex = m_gender,
-                        age = m_age,
-                        bib = runnerData[dataLocation[4]],
-                        team = runnerData[dataLocation[5]],
-                        city = runnerData[dataLocation[6]],
-                        state = runnerData[dataLocation[7]],
-                        country = runnerData[dataLocation[8]],
-                        overallPlace= runnerData[dataLocation[9]],
-                        genderPlace= runnerData[dataLocation[10]],
-                        agePlace= runnerData[dataLocation[11]],
-                        netTime= runnerData[dataLocation[12]],
-                        pacePerMile= runnerData[dataLocation[13]],
-                        AGTime= runnerData[dataLocation[14]],
-                        AGGenderPlace= runnerData[dataLocation[15]],
-                        AGPercent= runnerData[dataLocation[16]],
-                        )
+            item.append(NyrrStatsItem(raceName = rn[0].lower(), 
+                location=m_location.lower(), 
+                distMiles = m_distMiles.lower(), 
+                date = m_date.lower(), 
+                temp = m_temp.lower(), 
+                humidity = m_humidity.lower(),
+                lastName = runnerData[dataLocation[1]].lower(),
+                firstName = runnerData[dataLocation[2]].lower(),
+                sex = m_gender.lower(),
+                age = m_age.lower(),
+                bib = runnerData[dataLocation[4]].lower(),
+                team = runnerData[dataLocation[5]].lower(),
+                city = runnerData[dataLocation[6]].lower(),
+                state = runnerData[dataLocation[7]].lower(),
+                country = runnerData[dataLocation[8]].lower(),
+                overallPlace= runnerData[dataLocation[9]].lower(),
+                genderPlace= runnerData[dataLocation[10]].lower(),
+                agePlace= runnerData[dataLocation[11]].lower(),
+                netTime= runnerData[dataLocation[12]].lower(),
+                pacePerMile= runnerData[dataLocation[13]].lower(),
+                AGTime= runnerData[dataLocation[14]].lower(),
+                AGGenderPlace= runnerData[dataLocation[15]].lower(),
+                AGPercent= runnerData[dataLocation[16]].lower(),
+                ))
+
             k = k + 1
             data =  hxs.select('//table[2]/tr[' + str(k) + ']/td[1]/text()').extract()
-
-        return [item]
+            #print data
+            
+        return item + req
 
     def whichDataMember(self, member):
         myDict = {'Last Name': 1,
@@ -254,7 +220,3 @@ class NYRRSpider(BaseSpider):
         else:
             return -1
 
-    #want to do something like this
-    #food = 'bread'
-    #vars()[food] = 123
-    #print bread  # --> 123
